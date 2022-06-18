428ec30a8452748f544ac6e306fc084d6007f7aa
Balint Lendvai <lendvai.balint@gmail.com>
2022-06-18 23:31:40 +0200

    feat: adds content script to inject script.js to parent page

---
+++ contentScript.js
@@ -0,0 +1,7 @@
+var s = document.createElement('script');
+s.src = chrome.runtime.getURL('script.js');
+s.type="module";
+s.onload = function() {
+  this.remove();
+};
+(document.head || document.documentElement).appendChild(s);
